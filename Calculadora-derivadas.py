4995331470da610e337118064e5063260438bb3b
Pedro Mari <pedrobmari@gmail.com>
2025-12-29 13:58:08 -0300

    Opção 5 feita parcialmente (erro de lógica)

--- Calculadora-derivadas.py
+++ Calculadora-derivadas.py
@@ -12,7 +12,9 @@ estilo_invertido = "\033[7m"
 # Área de funções =========================================================================================
 
 def potencia(equacao):    
-    equacao = str(equacao)
+    equacao = str(equacao) #Variável que armazena o monômio a ser derivado
+
+    #Separa os elementos (coeficiente e expoente) do monômio
     try:
         if ("x" in equacao):
             resp = list(map(int, equacao.split("x^")))
@@ -31,9 +33,11 @@ def potencia(equacao):
         except:
             expoente = 1
 
+    #Processo de derivação sobre a regra da potência
     multiplicando = multiplicando*expoente
     expoente -= 1
 
+    #Simplifica a visualização dos resultados (não altera os valores matemáticos)
     if (expoente == 0):
         return multiplicando
     elif (expoente == 1):
@@ -42,20 +46,20 @@ def potencia(equacao):
         return str("{}x^{}".format(multiplicando, expoente))
 
 def soma_sub(sinal, num1, num2): 
-    resp = []
+    resp = [] #Lista que armazena os monômios derivados
     resp.append(potencia(num1))
     resp.append(potencia(num2))
 
     return str(f"{resp[0]} {sinal} {resp[-1]}")
 
 def multiplicacao(num1, num2):
-    nums = {}
-    resp = [potencia(num1), num2, num1, potencia(num2)]
+    resp = [potencia(num1), num2, num1, potencia(num2)] # Lista que organiza a ordem dos monômios e o que é derivado 
+    nums = {} # Dicionário que guarda os elementos de cada monômio separado para os cálculos
+    resp_final = {} # Armazena o resultado final
 
-    # SEPARADOR
+    # Separa os coeficientes (números que multiplicam as variáveis) e os expoentes da incógnita
     for item in range(len(resp)):
-        num = str(resp[item])
-        resp_final = {}
+        num = str(resp[item]) # Monômio (1 por vez)
         cont = 0
 
         if ("x" in num):
@@ -72,14 +76,16 @@ def multiplicacao(num1, num2):
             multiplicando = int(num)
             expoente = 0    
         
-        nums[f"num[{item}]"] = (multiplicando, expoente)
+        nums[f"num[{item}]"] = (multiplicando, expoente) # Adiciona os elementos separados em lista dentro do dicionário
 
+    # Realiza a multiplicação entre os monômios (multiplicando os coeficientes - números - e somando os expoentes)
     for i in range(0, 4, 2):
         multiplicando = (nums[f"num[{i}]"][0]) * (nums[f"num[{i+1}]"][0])
         expoente = (nums[f"num[{i}]"][1]) + (nums[f"num[{i+1}]"][1])
         resp_final[f"num[{cont}]"] = (multiplicando, expoente)
         cont += 1
 
+    #Verifica se é possível somar os monômios, retornando 2 possíveis casos (com soma e sem)
     if (resp_final["num[0]"][1] == resp_final["num[1]"][1]):
         multiplicando = (resp_final["num[0]"][0]) + (resp_final["num[1]"][0])
         expoente = (resp_final["num[0]"][1])
@@ -87,9 +93,50 @@ def multiplicacao(num1, num2):
     else:
         return (resp_final)
 
-# def quociente():
-#     print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
-    
+# def quociente(num1, num2):
+#     nums = [potencia(num1), num2, num1, potencia(num2), num2] # Lista que organiza a ordem dos monômios e o que é derivado 
+#     resp = {} # Dicionário que guarda os elementos de cada monômio separado para os cálculos
+#     numerador = {} # Armazena o resultado final
+
+#     # Separa os coeficientes (números que multiplicam as variáveis) e os expoentes da incógnita
+#     for item in range(len(nums)):
+#         num = str(nums[item]) # Monômio (1 por vez)
+#         cont = 0
+
+#         if ("x" in num):
+#             posicao_x = num.index("x")
+#             try:
+#                 multiplicando = int(num[:posicao_x])
+#             except:
+#                 multiplicando = 1
+#             try:
+#                 expoente = int(num[(posicao_x + 2):])
+#             except:
+#                 expoente = 1
+#         else:
+#             multiplicando = int(num)
+#             expoente = 0    
+        
+#         resp[f"num[{item}]"] = (multiplicando, expoente) # Adiciona os elementos separados em lista dentro do dicionário 
+        
+#     for i in range(0, 4, 2):
+#         multiplicando = (resp[f"num[{i}]"][0]) * (resp[f"num[{i+1}]"][0])
+#         expoente = (resp[f"num[{i}]"][1]) + (resp[f"num[{i+1}]"][1])
+#         numerador[f"num[{cont}]"] = (multiplicando, expoente)
+#         cont += 1
+
+#     #Verifica se é possível subtrair os monômios, retornando 2 possíveis casos (com subtração e sem)
+#     if (numerador["num[0]"][1] == numerador["num[1]"][1]):
+#         multiplicando = (numerador["num[0]"][0]) - (numerador["num[1]"][0])
+#         expoente = (numerador["num[0]"][1])
+#         resp_numerador = [multiplicando, expoente]
+#     else:
+#         resp_numerador = (numerador)
+
+#     denominador = (resp['num[4]'][0], resp['num[4]'][1] * 2)
+
+#     return (resp_numerador, denominador)
+
 # def cadeia():
 #     print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
 
@@ -98,7 +145,7 @@ def multiplicacao(num1, num2):
 while True:
     print(f"\n{"{} Calculadora de derivadas {}":=^60}".format(estilo_negrito, estilo_normal)) # Título
 
-    # Opções de operações (1 à 5)
+    # Opções de operações (1 à 5) para escolher
     try:
         operacao = int(input('''
     - Escolha a operação que deseja calcular:                      
@@ -111,16 +158,17 @@ while True:
     except:
         operacao = 0 # Caso seja digitado algo que não seja possível converter para número
 
-    # Verifica se a opção escolhida é válida, direcionando se sim, e, senão, retornando erro
+    # Verifica se a opção escolhida é válida, direcionando ou retornando erro
     match (operacao):
         case 1: # POTENCIAÇÃO
             print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
+
             equacao = input("{} - Digite a função a ser derivada: {}".format(estilo_negrito, estilo_normal))
-            pot = f"{estilo_negrito}{potencia(equacao)}{estilo_normal}"
+            pot = f"{estilo_negrito}{potencia(equacao)}{estilo_normal}" 
 
             print(" •{} A resposta é: {}{}".format(estilo_sublinhado, pot, estilo_normal))
 
-        case 2: # SOMA | SUBTRAÇÃO
+        case 2: # SOMA e SUBTRAÇÃO
             print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
 
             sinal = str(input("{} - Escolha a operação [+/-]: {}".format(estilo_negrito, estilo_normal)))
@@ -137,16 +185,25 @@ while True:
             num2 = input("{} - Digite o 2º número: {}".format(estilo_negrito, estilo_normal))
             mult = multiplicacao(num1, num2)
 
-            if (isinstance(mult, list)):
+            # Escreve o resultado na tela de acordo com a possibilidade de somar u não os monômios multiplicados
+            if (isinstance(mult, list)): 
                 mult = f"{mult[0]}x^{mult[-1]}"
             elif (isinstance(mult, dict)):
                 mult = f"{mult["num[0]"][0]}x^{mult["num[0]"][1]} . {mult["num[1]"][0]}x^{mult["num[1]"][1]}"
 
             print(" •{} A resposta é: {}{}".format(estilo_sublinhado, mult, estilo_normal))
 
-        # case 4: # QUOCIENTE
-        #     print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
+        case 4: # QUOCIENTE
+            print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
 
+            num1 = input("{} - Digite o 1º número: {}".format(estilo_negrito, estilo_normal))
+            num2 = input("{} - Digite o 2º número: {}".format(estilo_negrito, estilo_normal))
+            divi = quociente(num1, num2)
+
+            print('''
+ •{} A resposta é: {}{}
+                   {} '''.format(estilo_sublinhado, divi[0], divi[1], estilo_normal))
+            
         # case 5: # CADEIA
         #     print(f"\n{estilo_negrito}", "--" * 27, " {} \n".format(estilo_normal))
 
@@ -172,5 +229,5 @@ while True:
         break
     else: 
         os.system('cls')
-        print("\n {}***Tentativa inválida. Tente novamente {}\n".format(estilo_invertido, estilo_normal))
+        print("\n {}***Resposta inválida. Tente novamente. {}\n".format(estilo_invertido, estilo_normal))
         continue 
